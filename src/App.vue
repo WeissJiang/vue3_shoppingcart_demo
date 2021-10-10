0a554435f0a02988c54c666f54a26a13c1f4ac4d
weiss7777 <meweissjiang@gmail.com>
2021-10-10 15:10:34 +1100

    added shoppingcart project

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div @click="toggleSideBar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
+  </header>
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+
+  <Sidebar
+    v-if="showSideBar"
+    :toggle="toggleSideBar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+import food from './food.json'
+
+export default {
+  components: {
+    Sidebar
+  },
+  data () {
+    return {
+      showSideBar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, index) {
+      if (!this.cart[name]) {
+        this.cart[name] = 0
+      }
+      this.cart[name] += this.inventory[index].quantity
+      // console.log(this.inventory);
+      this.inventory[index].quantity = 0
+    },
+    toggleSideBar () {
+      this.showSideBar = !this.showSideBar
+      // console.log(this.showSideBar);
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
+}
+</script>
